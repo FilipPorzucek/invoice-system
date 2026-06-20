394dcaa159b54f1ec961224c5424257ba6116af2
FilipPorzucek <filipporzucek81@gmail.com>
2026-06-20 15:15:54 +0200

    improve imports

--- api/api/src/main/java/pl/cdv/api/controller/InternalInvoiceController.java
+++ api/api/src/main/java/pl/cdv/api/controller/InternalInvoiceController.java
@@ -3,16 +3,14 @@ package pl.cdv.api.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.cdv.api.dto.InvoiceDto;
 import pl.cdv.api.services.InvoiceService;
 
 @RestController
 @RequestMapping("/api/internal/invoices")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class InternalInvoiceController {
 
     private final InvoiceService invoiceService;
@@ -20,9 +18,7 @@ public class InternalInvoiceController {
     @PostMapping
     public ResponseEntity<String> receiveInvoiceFromOcr(@RequestBody InvoiceDto dto){
         System.out.println("Otrzymano zadanie post");
-
         invoiceService.processInvoiceFromOcr(dto);
-
         return ResponseEntity.ok("Zapisano fakture w bazie danych");
     }
 
